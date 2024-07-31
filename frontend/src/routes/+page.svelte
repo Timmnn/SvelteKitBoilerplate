864eba06f9a01c49f284d51b8c928117315e0ec6
timmn <timmmmnn@gmail.com>
2024-07-31 15:20:56 +0200

    Added Tailwind + Shadcnui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>
-   Page
-</h1>
+<script lang="ts">
+   import { Button } from "$components/ui/button";
+ </script>
+  
+<main class="p-4 flex flex-col items-center gap-4">
+  <h1 class="text-4xl font-bold text-center">
+    Svelte-Kit Boilerplate
+  </h1>
+ <Button>Shadcn</Button>
+</main>
